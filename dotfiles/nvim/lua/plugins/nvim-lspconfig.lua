89c6fb9f94592d3ae8674d8f17ae6f23ee17fdfb
Antti Kupila <akupila@gmail.com>
2023-09-12 22:07:04 +0200

    nvim: enable typescript lsp

--- dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
@@ -39,6 +39,9 @@ return {
       },
     }
 
+    lspconfig.tsserver.setup {
+    }
+
     vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
