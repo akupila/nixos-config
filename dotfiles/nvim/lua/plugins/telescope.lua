7aac40970422519e82dabc3f18ceb69549081d73
Antti Kupila <akupila@gmail.com>
2023-12-28 10:18:10 +0200

    nvim: telescope: find all files with ctrl-p

--- dotfiles/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -8,8 +8,7 @@ return {
   cmd = 'Telescope',
   init = function()
     vim.keymap.set("n", "<space>", "<cmd>Telescope buffers<cr>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<C-p>", "<cmd>Telescope git_files<cr>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<C-_>", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { noremap = true, silent = true }) -- C-/
+    vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>", { noremap = true, silent = true })
     vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true, silent = true })
     vim.keymap.set("n", "<leader>fr", "<cmd>Telescope lsp_references<cr>", { noremap = true, silent = true })
     vim.keymap.set("n", "<leader>fi", "<cmd>Telescope lsp_implementations<cr>", { noremap = true, silent = true })
