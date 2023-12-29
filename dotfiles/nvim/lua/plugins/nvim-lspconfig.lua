e2110670eca0a2c255b696d35b9a921ea642ebf5
Antti Kupila <akupila@gmail.com>
2023-12-29 12:10:59 +0200

    nvim: clean up lsp, add hover border

--- dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,6 +3,13 @@ return {
   config = function()
     local lspconfig = require("lspconfig")
 
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+      vim.lsp.handlers.hover, {
+        border = 'rounded',
+        max_width = 100,
+      }
+    )
+
     lspconfig.gopls.setup {
       settings = {
         gopls = {
@@ -55,36 +62,18 @@ return {
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
     vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
-    -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
     vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
       callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
         local opts = { buffer = ev.buf }
-        -- local client = vim.lsp.get_client_by_id(ev.data.client_id)
-
-        -- if client.supports_method("textDocument/formatting") then
-        --   vim.api.nvim_clear_autocmds({ group = augroup, buffer = ev.buf })
-        --   vim.api.nvim_create_autocmd("BufWritePre", {
-        --     group = augroup,
-        --     buffer = ev.buf,
-        --     callback = function()
-        --       vim.lsp.buf.format()
-        --     end,
-        --   })
-        -- end
-
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', '<leader>gd', ':vsplit | lua vim.lsp.buf.definition()<CR>', opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         vim.keymap.set({ 'n', 'i' }, '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<leader>wl', function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
         vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
         vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
