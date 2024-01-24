18ff76b72aea970575a7ac2160bb63de239cffb1
Antti Kupila <akupila@gmail.com>
2024-01-24 17:44:01 +0200

    nvim: more visible line numbers

--- dotfiles/nvim/lua/plugins/ayu.lua
+++ dotfiles/nvim/lua/plugins/ayu.lua
@@ -1,6 +1,14 @@
 return {
   "Shatur/neovim-ayu",
   config = function()
-    require("ayu").colorscheme()
+    local ayu = require('ayu')
+
+    ayu.setup({
+      overrides = {
+        LineNr = { fg = '#2e3644' }, -- Slightly more visible
+      }
+    })
+
+    ayu.colorscheme()
   end
 }
