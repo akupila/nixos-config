cc94ce6468f90a73bb416d06667e9aebd8c453d8
Antti Kupila <akupila@gmail.com>
2024-05-13 15:53:54 +0300

    nvim: set filemerge-like keys for diffview

--- dotfiles/nvim/lua/plugins/diffview.lua
+++ dotfiles/nvim/lua/plugins/diffview.lua
@@ -1,3 +1,25 @@
 return {
-  'sindrets/diffview.nvim'
+  'sindrets/diffview.nvim',
+  config = function()
+    local actions = require("diffview.actions")
+
+    require("diffview").setup({
+      view = {
+        merge_tool = {
+          layout = "diff3_mixed",
+        },
+      },
+      keymaps = {
+        view = {
+          { "n", "<up>", actions.prev_conflict, {
+            desc =
+            "In the merge-tool: jump to the previous conflict"
+          } },
+          { "n", "<down>",  actions.next_conflict,             { desc = "In the merge-tool: jump to the next conflict" } },
+          { "n", "<left>",  actions.conflict_choose("ours"),   { desc = "Choose the OURS version of a conflict" } },
+          { "n", "<right>", actions.conflict_choose("theirs"), { desc = "Choose the THEIRS version of a conflict" } },
+        },
+      },
+    })
+  end
 }
