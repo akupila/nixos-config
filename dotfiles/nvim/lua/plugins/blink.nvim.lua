eac75ea3edf590f6b58b6bc90d66fd433dd09fdd
Antti Kupila <antti.kupila@iceye.com>
2025-03-12 09:23:58 +0200

    nvim: fix blink config

--- dotfiles/nvim/lua/plugins/blink.nvim.lua
+++ dotfiles/nvim/lua/plugins/blink.nvim.lua
@@ -37,7 +37,10 @@ return {
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
-      cmdline = {}
+    },
+
+    cmdline = {
+      sources = {},
     },
 
     signature = { enabled = true }
