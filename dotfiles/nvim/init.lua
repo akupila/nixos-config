ea81433ea5023b46d4d2ce213c599909fc687676
Antti Kupila <antti.kupila@iceye.com>
2025-04-06 13:11:26 +0300

    nvim: add diagnostics keys descriptions

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -111,10 +111,9 @@ vim.diagnostic.config({
   update_in_insert = true,
   severity_sort = true,
 })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostics' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
 
 -- Allow saving with uppercase W so that shift-; for : doesn't accidentally also capitalize w
 vim.api.nvim_create_user_command('W', 'w', {})
