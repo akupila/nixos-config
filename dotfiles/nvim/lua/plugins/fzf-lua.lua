90bb09d13759c810693f666a29072ac9d388331c
Antti Kupila <antti.kupila@iceye.com>
2025-04-06 13:11:46 +0300

    nvim: replace telescope with fzf-lua

---
+++ dotfiles/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,24 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = {},
+  config = function()
+    local fzf = require("fzf-lua")
+    fzf.setup()
+
+    vim.api.nvim_set_keymap("n", "<C-\\>", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {
+      desc = "Fuzzy find buffers",
+    })
+    vim.api.nvim_set_keymap("n", "<C-k>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {
+      desc = "Fuzzy find builtin",
+    })
+    vim.api.nvim_set_keymap("n", "<C-p>", [[<Cmd>lua require"fzf-lua".files()<CR>]], {
+      desc = "Fuzzy find files",
+    })
+    vim.api.nvim_set_keymap("n", "<C-l>", [[<Cmd>lua require"fzf-lua".live_grep_glob()<CR>]], {
+      desc = "Fuzzy find live grep",
+    })
+    vim.api.nvim_set_keymap("n", "<C-g>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {
+      desc = "Fuzzy find grep project",
+    })
+  end
+}
