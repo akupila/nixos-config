e2fbd33c5c45eb0800706366866afbdf67a65e95
Antti Kupila <antti.kupila@iceye.com>
2025-12-17 11:55:56 +0200

    darwin: faster key repeat

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -18,6 +18,13 @@
   # Enable touch id for sudo.
   security.pam.services.sudo_local.touchIdAuth = true;
 
+  system.primaryUser = user;
+
+  system.defaults.NSGlobalDomain = {
+    InitialKeyRepeat = 10; # Normal minimum is 15 (225ms)
+    KeyRepeat = 1; # Normal minimum is 2 (30ms)
+  };
+
   # Used for backwards compatibility, check changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
