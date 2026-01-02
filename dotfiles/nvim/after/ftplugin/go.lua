9f09450612f827e71b8cfc0082c3ff744bb23e68
Antti Kupila <antti.kupila@iceye.com>
2026-01-02 12:19:45 +0200

    nvim: fix Go selection

--- dotfiles/nvim/after/ftplugin/go.lua
+++ dotfiles/nvim/after/ftplugin/go.lua
@@ -1,3 +1,6 @@
 -- Prevent using gofmt for formatting (provided by Neovim's native Go ftplugin)
 vim.opt_local.formatprg = "" -- gq
 vim.opt_local.formatexpr = "" -- LSP range formatting
+
+-- Remove '.' from iskeyword so viw selects 'Info' not 'slog.Info'
+vim.opt_local.iskeyword:remove(".")
