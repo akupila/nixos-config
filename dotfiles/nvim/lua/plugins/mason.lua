e028007af74393e33eff7efda75fc7e3ab96a5ca
Antti Kupila <antti.kupila@iceye.com>
2026-03-31 11:37:45 +0300

    mason: remove some clangd and terraformls by default

--- dotfiles/nvim/lua/plugins/mason.lua
+++ dotfiles/nvim/lua/plugins/mason.lua
@@ -4,10 +4,8 @@ vim.pack.add({ "https://github.com/williamboman/mason-lspconfig.nvim" })
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = {
-		"clangd",
 		"gopls",
 		"lua_ls",
 		"nil_ls",
-		"terraformls",
 	},
 })
