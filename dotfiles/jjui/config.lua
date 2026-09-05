987447be5ac303b2c72a391d356f678602c699a1
Antti Kupila <antti.kupila@iceye.com>
2026-09-05 08:19:19 +0300

    jjui: add ctrl+n to create new change after selected

--- dotfiles/jjui/config.lua
+++ dotfiles/jjui/config.lua
@@ -7,4 +7,17 @@ function setup(config)
       flash("Copied change id: " .. id)
     end
   end, { key = "Y", scope = "revisions", desc = "copy change id" })
+
+  config.action("new-after-selected", function()
+    local id = context.change_id()
+    if not id then
+      return
+    end
+    local _, err = jj("new", "-A", id)
+    if err then
+      flash({ text = err, error = true })
+      return
+    end
+    revisions.refresh()
+  end, { key = "ctrl+n", scope = "revisions", desc = "new change after selected" })
 end
